e561e8a34b15a8afb27bf0ab751939e79ca23489
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-03-14 14:07:31 +0000

    default values added

--- Greetr.js
+++ Greetr.js
@@ -2,4 +2,20 @@
 
 (function (global, $) {
 
+  var Greetr = function(firstName, lastName, language) {
+    return new Greetr.init(firstName, lastName, language);
+  };
+
+  Greetr.prototype = {};
+
+  Greetr.init = function(firstName, lastName, language) {
+
+    var self = this;
+    self.firstName = firstName || '';
+    self.lastName = lastName || '';
+    self.language = language || 'en';
+  };
+
+  Greetr.init.prototype = Greetr.prototype; 
+
 }(window, jQuery));
